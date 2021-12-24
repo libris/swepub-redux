7a35d8c767e00cb0f2546bc5021ebe32c18a76f3
Jannis Mohlin Tsiroyannis <jannis.mohlintsiroyannis@kb.se>
2021-12-24 21:19:37 +0100

    sqlite stuff

--- pipeline/deduplicate.py
+++ pipeline/deduplicate.py
@@ -8,7 +8,7 @@ def deduplicate():
     cursor = get_cursor()
     for row in cursor.execute("""
     SELECT
-        maintitle.maintitle, count(converted.oai_id)
+        maintitle.maintitle, group_concat(converted.id)
     FROM
         maintitle
     LEFT JOIN
@@ -16,47 +16,9 @@ def deduplicate():
     GROUP BY
         maintitle.maintitle;
     """):
-        print(row)
+        print(row)                
 
-    # map from oai_id -> record, for all of swepub
-    #records_by_id = {}
-
-    # map words (found within each publications main title), to lists of oai_ids
-    # of the records containing those words.
-    #index = {}
-    
-    #for entry in os.scandir('./output/raw/'):
-    #    if entry.is_file():
-    #        with open(entry, "r") as f:
-    #            for line in f.readlines():
-    #                record = json.loads(line)
-
-                    # The oai-id
-    #                records_by_id[record["@id"]] = record
-
-                    # For each word in the title
-    #                for title in record["instanceOf"]["hasTitle"]:
-    #                    for word in re.findall(r"\w+", title["mainTitle"]):
-    #                        lowcaseword = str.lower(word)
-    #                        if not lowcaseword in stopwords:
-    #                            if not lowcaseword in index:
-    #                                index[lowcaseword] = [] 
-    #                            index[lowcaseword].append(record["@id"])
-
-    #for oai_id, record in records_by_id.items():
-    #    candidate_duplicates = [oai_id]
-
-    #    for title in record["instanceOf"]["hasTitle"]:
-    #        for word in re.findall(r"\w+", title["mainTitle"]):
-    #            lowcaseword = str.lower(word)
-    #            if not lowcaseword in stopwords:
-    #                for candidate_id in index[lowcaseword]:
-    #                    candidate_duplicates.append(candidate_id)
-
-    #    if len(candidate_duplicates) > 1:
-    #        print(f"check for dups: {len(candidate_duplicates)}")
-                    
-
-# TEMP
-open_existing_storage()
-deduplicate()
+# For debugging
+if __name__ == "__main__":    
+    open_existing_storage()
+    deduplicate()
---
+++ pipeline/storage.py
@@ -0,0 +1,54 @@
+import sqlite3
+from sqlite3.dbapi2 import connect
+import json
+import os
+
+sqlite_path = "./swepub.sqlite3"
+
+connection = None
+
+def clean_and_init_storage():
+    if os.path.exists(sqlite_path):
+        os.remove(sqlite_path)
+    global connection
+    connection = sqlite3.connect(sqlite_path)
+    cursor = connection.cursor()
+    cursor.execute("""
+    CREATE TABLE converted (
+        id INTEGER PRIMARY KEY,
+        cluster INTEGER,
+        oai_id TEXT,
+        data TEXT
+    );
+    """)
+    cursor.execute("""
+    CREATE TABLE maintitle (
+        maintitle TEXT,
+        converted_id INTEGER,
+        FOREIGN KEY (converted_id) REFERENCES converted(id)
+    );
+    """)
+    connection.commit()
+
+def open_existing_storage():
+    global connection
+    connection = sqlite3.connect(sqlite_path)
+
+def store_converted(converted):
+    cursor = connection.cursor()
+
+    print(f'Inserting with oai_id {converted["@id"]}')
+
+    converted_id = connection.execute("""
+    INSERT INTO converted(cluster, oai_id, data) VALUES(?, ?, ?);
+    """, (-1, converted["@id"], json.dumps(converted))).lastrowid
+
+    for title in converted["instanceOf"]["hasTitle"]:
+        connection.execute("""
+        INSERT INTO maintitle VALUES (?, ?)
+        """, (title["mainTitle"], converted_id))
+
+    connection.commit()
+
+def get_cursor():
+    return connection.cursor()
