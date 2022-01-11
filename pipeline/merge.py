8f058a0589541fbfa7d2fe3a7c5e9b604410306a
Jannis Mohlin Tsiroyannis <jannis.mohlintsiroyannis@kb.se>
2022-01-11 22:06:42 +0100

    Do parallell publication merging and auditing.

--- pipeline/merge.py
+++ pipeline/merge.py
@@ -1,29 +1,16 @@
-import threading
 from audit import audit
 from storage import commit_sqlite, get_cursor, open_existing_storage
 from merger import PublicationMerger
 from publication import Publication
-from multiprocessing import Process, Pool
-from threading import Thread, Event
+from multiprocessing import Pool
 import time
 import json
-import sqlite3
 
 def merge():
     # Set up batching
     batch = []
     tasks = []
     in_batch = 0
-
-    # First setup the consumer thread, which will take work done by other
-    # processes, and write it to storage. This needs to be done using a 
-    # thread, because we can't cross the process boundry. If done from
-    # another process, writing would be blocked because there's an open
-    # query reading concurrently in _this_ process, and sqlite3 uses
-    # per-process read/write locks on the database file.
-    #queue = Queue()
-    #consumer_thread = threading.Thread(target=_consume, args=(queue,))
-    #consumer_thread.start()
 
     with Pool(processes=16) as pool:
 
@@ -51,29 +38,31 @@ def merge():
                     i = n-1
                     while i > -1:
                         if tasks[i].ready():
-                            print("* CLEARING A PROCESS")
+                            #print("* CLEARING A PROCESS")
                             result = tasks[i].get()
-                            #print(f" subprocess returned: {result}")
-                            #print(f" subprocess returned: {isinstance(result[0], list)}")
-                            #for lol in result[0]:
-                            #    print(f"really? {lol[0]}\n\n") # GAVE CLUSTERID!
-                            #print(f" subprocess returned: {result[0]}")
-
-                            #print(f"result len = {len(result)}\n\n")
-                            for cluster in result[0]:
-                                cluster_id = cluster[0]
-                                merged_data = cluster[1]
-                                print(f"really? {cluster_id} -> {isinstance(merged_data, str)} {merged_data} \n\n")
-                                inner_cursor.execute("""
-                                INSERT INTO finalized(cluster_id, data) VALUES(?, ?);
-                                """, (cluster_id, json.dumps(merged_data)))
-                                #print(" SUCCESSFUL WRITE!")
-                                commit_sqlite() # TEEEEEEEEEEEEMP!!
+                            write_results(result, inner_cursor)
 
                             del(tasks[i])
                         i -= 1
                 tasks.append(pool.map_async(_handle, (batch,)))
-    
+                batch = []
+
+        if len(batch) > 0:
+            tasks.append(pool.map_async(_handle, (batch,)))
+        for task in tasks:
+            while not task.ready():
+                time.sleep(0)
+            result = task.get()
+            write_results(result, inner_cursor)
+
+def write_results(result, inner_cursor):
+    for cluster in result[0]:
+        cluster_id = cluster[0]
+        merged_data = cluster[1]
+        #print(f"really? {cluster_id} -> {isinstance(merged_data, str)} {merged_data} \n\n")
+        inner_cursor.execute("""
+        INSERT INTO finalized(cluster_id, data) VALUES(?, ?);
+        """, (cluster_id, json.dumps(merged_data)))
     commit_sqlite()
 
 def _handle(batch): # batch is a list of cluster rows
@@ -92,7 +81,6 @@ def _handle(batch): # batch is a list of cluster rows
         audit(union_publication.body)
 
         results.append( (cluster_id, union_publication.body) )
-    print("Batch done!")
     return results
 
 
