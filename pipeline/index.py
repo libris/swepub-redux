afe9db19481e0b8f8ada6bde8795e4d5492a417d
Anders Jensen-Urstad <anders.jensen-urstad@kb.se>
2022-01-21 18:53:37 +0100

    Add code for creating search tables

---
+++ pipeline/index.py
@@ -0,0 +1,93 @@
+import json
+from storage import commit_sqlite, get_cursor
+from bibframesource import BibframeSource
+
+OUTPUT_TYPE_PREFIX = 'https://id.kb.se/term/swepub/'
+
+
+def generate_search_tables():
+    cursor = get_cursor()
+
+    for row in cursor.execute("SELECT id, cluster_id, data FROM finalized LIMIT 50"):
+        inner_cursor = get_cursor()
+        finalized_id = row[0]
+        cluster_id = row[1]
+        doc = BibframeSource(json.loads(row[2]))
+
+        inner_cursor.execute("""
+            INSERT INTO search_single_values(
+            finalized_id, year, content_marking, publication_status, swedish_list
+            ) VALUES(
+            ?, ?, ?, ?, ?
+            )
+            """, (
+            finalized_id,
+            doc.publication_year,
+            doc.content_marking,
+            get_publication_status(doc),
+            doc.is_swedishlist
+        ))
+
+        for doi in doc.doi:
+            inner_cursor.execute("INSERT INTO search_doi (finalized_id, doi) VALUES (?, ?)", (finalized_id, doi))
+
+        for gf in doc.output_types:
+            if gf.startswith(OUTPUT_TYPE_PREFIX):
+                gf_shortened = gf[len(OUTPUT_TYPE_PREFIX):]
+            else:
+                gf_shortened = gf
+            inner_cursor.execute("INSERT INTO search_genre_form (finalized_id, genre_form) VALUES (?, ?)", (finalized_id, gf_shortened))
+
+        for subject in [item for sublist in doc.uka_subjects.values() for item in sublist]:
+            inner_cursor.execute("INSERT INTO search_subject (finalized_id, subject) VALUES (?, ?)", (finalized_id, subject))
+
+        inner_cursor.execute("INSERT INTO search_fulltext_title_keywords (rowid, title, keywords) VALUES (?, ?, ?)", (
+                       finalized_id,
+                       doc.title,
+                       " ".join(doc.keywords)
+                       ))
+
+        for creator in doc.creators:
+            inner_cursor.execute("""
+                INSERT INTO search_creator(
+                finalized_id, orcid, family_name, given_name, local_id, local_id_by
+                ) VALUES(
+                ?, ?, ?, ?, ?, ?
+                )
+                """, (
+                finalized_id,
+                creator.get("ORCID", None),
+                creator.get("familyName", None),
+                creator.get("givenName", None),
+                creator.get("localId", None),
+                creator.get("localIdBy", None)
+            ))
+
+        # Get org code from candidate/duplicate publications. No need to go through the actual documents, because we've
+        # already put the code in the `converted` table.
+        inner_cursor.execute(
+            "SELECT co.source FROM converted co JOIN cluster cl ON co.id=cl.converted_id WHERE cl.cluster_id = ?", (cluster_id,))
+        sources = inner_cursor.fetchall()
+        for source in set([item for sublist in sources for item in sublist]):
+            inner_cursor.execute("INSERT INTO search_org (finalized_id, org) VALUES (?, ?)",
+                                 (finalized_id, source))
+
+        commit_sqlite()
+        inner_cursor.close()
+
+
+def get_publication_status(doc):
+    ps = doc.publication_status
+    if ps == "https://id.kb.se/term/swepub/Published":
+        return "published"
+    elif ps == "https://id.kb.se/term/swepub/EpubAheadOfPrintOnlineFirst":
+        return "epub"
+    elif ps in ["https://id.kb.se/term/swepub/Submitted",
+                "https://id.kb.se/term/swepub/Accepted",
+                "https://id.kb.se/term/swepub/InPress"]:
+        return "submitted"
+    return None
+
+
+def is_swedish_list(pub):
+    return "https://id.kb.se/term/swepub/swedishlist/peer-reviewed" in pub.genre_form
