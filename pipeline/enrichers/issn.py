67a3bd0877970cf88607a7f59521d46fc9e7f6a6
Anders Jensen-Urstad <anders.jensen-urstad@kb.se>
2022-03-21 12:41:32 +0100

    Fix deprecation warnings

--- pipeline/enrichers/issn.py
+++ pipeline/enrichers/issn.py
@@ -3,16 +3,16 @@ from pipeline.util import update_at_path, add_sibling_at_path, unicode_translate
 
 # flake8: noqa W504
 issn_regex = re.compile(
-    '(?:(?<!(\d[^0-9-])))' +  # Check that not preceded by digit or and not number or hyphen
-    '(?:(?<![0-9-]))' +       # Check that not preceded by digit
-    '([0-9]{4})' +            # Four consecutive numbers
-    '(-?)' +                  # Accept a correct delimiter if one exists
-    '[^0-9]*' +               # Any number of non-digit signs as delimiter
-    '([0-9]{3})' +            # Three consecutive numbers
-    '[^0-9]??' +              # optional non-numerical sign before the control number
-    '([0-9xX])' +             # Control number
-    '(?:(?![0-9-]))' +        # Make sure the code isn't followed by a number or hyphen
-    '(?:(?![ ]\d))'           # Check that not followed by non digit or hyphen and digit
+    r'(?:(?<!(\d[^0-9-])))' +  # Check that not preceded by digit or and not number or hyphen
+    r'(?:(?<![0-9-]))' +       # Check that not preceded by digit
+    r'([0-9]{4})' +            # Four consecutive numbers
+    r'(-?)' +                  # Accept a correct delimiter if one exists
+    r'[^0-9]*' +               # Any number of non-digit signs as delimiter
+    r'([0-9]{3})' +            # Three consecutive numbers
+    r'[^0-9]??' +              # optional non-numerical sign before the control number
+    r'([0-9xX])' +             # Control number
+    r'(?:(?![0-9-]))' +        # Make sure the code isn't followed by a number or hyphen
+    r'(?:(?![ ]\d))'           # Check that not followed by non digit or hyphen and digit
 )
 
 def recover_issn(body, field):
