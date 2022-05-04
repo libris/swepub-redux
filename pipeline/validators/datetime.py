bf030cf6ab61860fdf980d5a8af62b3b44e1df70
Anders Jensen-Urstad <anders.jensen-urstad@kb.se>
2022-05-04 11:50:11 +0200

    Remove unnecessary unicode validator from datetime

--- pipeline/validators/datetime.py
+++ pipeline/validators/datetime.py
@@ -1,16 +1,11 @@
 from datetime import datetime
 
-from pipeline.validators.shared import validate_base_unicode
 from pipeline.util import make_event, Validation, Enrichment
 
 
 def validate_format(dt):
     """Valid date formats are YYYY and YYYY-MM-DD, as specified in Swepub MODS specification."""
     if isinstance(dt, str):
-        if not validate_base_unicode(dt):
-            # field.events.append(make_event(type="validation", code="unicode", result="invalid", value=dt))
-            return False, "unicode"
-
         try:
             datetime.strptime(dt, "%Y")
             return True, "format"
