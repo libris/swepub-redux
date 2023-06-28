8c515e65bbaddc63c13b6a9adb9dc9618f6c0b1c
Andreas Ekström <didair@msn.com>
2023-06-28 15:30:10 +0200

    WIP matomo and add whatwg-fetch

--- service/vue3-client/src/main.ts
+++ service/vue3-client/src/main.ts
@@ -1,8 +1,10 @@
+import 'whatwg-fetch';
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import { FontAwesomeIcon, FontAwesomeLayers } from '@fortawesome/vue-fontawesome';
 
+import VueMatomo from 'vue-matomo';
 import App from './App.vue'
 import router from './router'
 import * as LayoutUtil from '@/utils/Layout'
@@ -13,6 +15,19 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+// only use Matomo if id is present, i.e in production mode
+// TODO: Figure out how we initialize something using values from store
+// if (store.getters.settings.matomoId) {
+// 	app.use(VueMatomo, {
+// 		host: 'https://analytics.kb.se',
+// 		siteId: store.getters.settings.matomoId,
+// 		trackerFileName: 'matomo',
+// 		disableCookies: true,
+// 		router,
+// 		enableLinkTracking: true,
+// 	});
+// }
 
 router.afterEach((route) => {
 	const { serviceName } = useSettingsStore();
