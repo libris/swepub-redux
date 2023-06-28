e8202f3ecb8b305fdeb7b5c0092154ddd9fa3b98
Andreas Ekström <didair@msn.com>
2023-06-28 14:14:07 +0200

    Move more files from vue-client

--- service/vue3-client/src/router/index.ts
+++ service/vue3-client/src/router/index.ts
@@ -17,6 +17,43 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
     }
+    // {
+    //   path: '/',
+    //   redirect: '/bibliometrics',
+    // },
+    // {
+    //   path: '/classify/:section?',
+    //   name: 'Classify',
+    //   meta: { title: 'Ämnesklassificering' },
+    //   component: () => import(/* webpackChunkName: "classify" */'@/views/Classify'),
+    // },
+    // {
+    //   path: '/bibliometrics/:section?',
+    //   name: 'Bibliometrics',
+    //   meta: { title: 'Bibliometri' },
+    //   props: (route) => ({ query: route.query }),
+    //   component: () => import(/* webpackChunkName: "bibliometrics" */'@/views/Bibliometrics'),
+    // },
+    // {
+    //   path: '/process/:source?/:service?/:subservice?', // ? = optional param
+    //   name: 'Process',
+    //   meta: { title: 'Databearbetning' },
+    //   props: (route) => ({ query: route.query, params: route.params }),
+    //   component: () => import(/* webpackChunkName: "process" */'@/views/Process'),
+    // },
+    // {
+    //   path: '/datastatus/:source?',
+    //   name: 'Datastatus',
+    //   meta: { title: 'Datastatus' },
+    //   props: (route) => ({ params: route.params, query: route.query }),
+    //   component: () => import(/* webpackChunkName: "process" */'@/views/Datastatus'),
+    // },
+    // {
+    //   path: '*',
+    //   name: 'NotFound',
+    //   meta: { title: 'Sidan kan inte hittas' },
+    //   component: () => import(/* webpackChunkName: "notfound" */'@/views/NotFound'),
+    // },
   ]
 })
 
