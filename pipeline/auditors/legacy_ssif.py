b17a73d8fb975bd39b3e40e4bfa0b13783b81f89
Anders Jensen-Urstad <anders.jensen-urstad@kb.se>
2024-12-05 19:02:46 +0100

    Also use closeMatch

--- pipeline/auditors/legacy_ssif.py
+++ pipeline/auditors/legacy_ssif.py
@@ -9,29 +9,25 @@ class LegacySSIFAuditor(BaseAuditor):
         self.name = LegacySSIFAuditor.__name__
 
     def audit(self, publication, audit_events, _harvest_cache, _session):
-        #old_classifications = publication.classifications
-
+        ssifs_not_migrated = set()
         for classification in publication.classifications:
-            description = get_description(classification["@id"])
-            if not description:
+            if not (description := get_description(classification["@id"])):
                 continue
-            is_replaced_bys = description.get("isReplacedBy", [])
-            if is_replaced_bys:
+            if is_replaced_bys := description.get("isReplacedBy", []):
                 if len(is_replaced_bys) == 1:
-                    # If there's exactly one possible replacement, use it
                     classification["@id"] = is_replaced_bys[0]["@id"]
                 else:
                     replaced_by_ids = list(map(lambda x: x["@id"].removeprefix(SSIF_BASE), is_replaced_bys))
                     level_3 = replaced_by_ids[0][:3]
                     if all(classification.startswith(level_3) for classification in replaced_by_ids):
-                        # All SSIF codes have the same level 3, so use it
                         classification["@id"] = f"{SSIF_BASE}{level_3}"
+                    elif (narrow_match := description.get("narrowMatch", [])) and len(narrow_match) == 1:
+                        classification["@id"] = narrow_match[0]["@id"]
+                    elif (close_match := description.get("closeMatch", [])) and len(close_match) == 1:
+                        classification["@id"] = close_match[0]["@id"]
                     else:
-                        narrow_match = description.get("narrowMatch", [])
-                        if len(narrow_match) == 1:
-                            classification["@id"] = narrow_match[0]["@id"]
-                        else:
-                            print("No SSIF 2011->2025 mapping possible")
-                            audit_events.add_event(self.name, "SSIF_2011_not_migrated", '', publication.classifications, '')
+                        ssifs_not_migrated.add(classification["@id"])
+        if ssifs_not_migrated:
+            audit_events.add_event(self.name, "SSIF_2011_not_migrated", '', list(ssifs_not_migrated), '')
 
         return publication, audit_events
